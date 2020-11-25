d8a9aee1e98ef878dfc4e1fe0331e460fe2de314
charlupuhh <62491397+charlupuhh@users.noreply.github.com>
2020-11-24 23:44:51 -0800

    first draft

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gymcari`,
+    description: `For calculating fees selling Gymshark through Mercari`,
+    author: `@charlie-lu1`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,10 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#DBF4A7`,
+        theme_color: `#DBF4A7`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/shark.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,69 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { TextField } from '@material-ui/core';
+// import Image from "../components/image"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const initialValues = {
+  price: 0,
+  sellPrice: 0,
+  discount: 0,
+  tax: 0.0925,
+  mercari: 0.1000,
+  paypalPercent: 0.029,
+  paypalFlat: 0.30
+}
+
+
+const IndexPage = () => {
+  
+  const [values, setValues] = useState(initialValues);
+
+  function handleChanges(e){
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleChangesPercent(e){
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value / 100
+    })
+  }
+
+  return (
+    <Layout>
+      <div className="container">
+
+        <div className="input-group">
+          <div className="inputs" id='input'>
+            <span className="input"><span className="icon">$</span><TextField id="outlined-basic" label="Gymshark Price:" name="price" variant="outlined" type="number" onChange={e => {handleChanges(e)}}/></span>
+            <span className="input"><span className="icon">$</span><TextField id="outlined-basic" label="Mercari Price:" name="sellPrice" variant="outlined" type="number" onChange={e => {handleChanges(e)}}/></span>
+          </div>
+
+          <div className="inputs" id='fees'>
+            <span className="input"><TextField id="outlined-basic" label="Discount:" name="discount" variant="outlined" type="number" defaultValue={(values["discount"] * 100).toFixed(2)} onChange={e => {handleChangesPercent(e)}}/></span>
+            <span className="input"><TextField id="outlined-basic" label="Tax:" name="tax" variant="outlined" type="number" defaultValue={values["tax"] * 100} onChange={e => {handleChangesPercent(e)}}/></span>
+            <span className="input"><TextField id="outlined-basic" label="Mercari Fees" name="mercari" variant="outlined" type="number" defaultValue={(values["mercari"] * 100).toFixed(2)} onChange={e => {handleChangesPercent(e)}}/></span>
+            <div>
+              <span className="input"><TextField id="outlined-basic" label="PayPal Fee" name="paypalPercent" variant="outlined" type="number" defaultValue={(values["paypalPercent"] * 100).toFixed(2)} onChange={e => {handleChangesPercent(e)}}/></span> + 
+              $<span className="input"><TextField id="outlined-basic" label="PayPal Fee" name="paypalFlat" variant="outlined" type="number" defaultValue={values["paypalFlat"]} onChange={e => {handleChanges(e)}}/></span>
+            </div>
+          </div>
+        </div>
+
+        <div className="output-group">
+
+          <p>Breakeven: ${(values["price"] * (1 - values["discount"]) * (1 + values["tax"]) / (1 - values["mercari"] - values["paypalPercent"]) + values["paypalFlat"]).toFixed(2)}</p>
+          {/* values["sellPrice"] */}
+          <p>Profit: {Math.max(0, values["sellPrice"] * (1- values["mercari"] - values["paypalPercent"]) - values["paypalFlat"] - values["price"]).toFixed(2) }</p>
+        </div>
+      </div>
+
+    </Layout>
+  )
+}
 
 export default IndexPage
